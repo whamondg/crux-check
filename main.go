60beefbf357552f336b9191e2cd87f3224b3d2dc
whamondg <gordon.whamond@googlemail.com>
2020-11-22 16:03:36 +0000

    separate printing from metric scoring

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -19,51 +20,71 @@ func assessMetric(metric Metric) (bool, error) {
 	return p75Num < thresholdNum, nil
 }
 
+/*
+MetricAssessment contains an evaluation of a CrUX measurment scored against its target threshold
+*/
+type MetricAssessment struct {
+	name      string
+	valid     bool
+	p75       string
+	threshold string
+	score     string
+}
+
+func validMetric(metric Metric) bool {
+	return len(metric.Histogram) > 0
+}
+
+func metricScore(p75 json.Number, threshold json.Number) string {
+	p75Num, err := p75.Float64()
+	if err != nil {
+		return "ERROR - Failed to convert p75 value to number"
+	}
+
+	thresholdNum, err := threshold.Float64()
+	if err != nil {
+		return "ERROR - Failed to convert threshold value to number"
+	}
+
+	if p75Num < thresholdNum {
+		return "Pass"
+	} else {
+		return "Fail"
+	}
+}
+
+func createMetricAssesment(name string, metric Metric) MetricAssessment {
+	var p75, threshold, score string
+	if validMetric(metric) {
+		p75 = string(metric.Percentiles.P75)
+		threshold = string(metric.Histogram[0].End)
+		score = metricScore(metric.Percentiles.P75, metric.Histogram[0].End)
+	}
+	return MetricAssessment{p75: p75, threshold: threshold, name: name, score: score}
+}
+
+func assessCoreWebVitals(cruxRecord CruxRecord) []MetricAssessment {
+	return []MetricAssessment{
+		createMetricAssesment("CLS", cruxRecord.Record.Metrics.CLS),
+		createMetricAssesment("FID", cruxRecord.Record.Metrics.FID),
+		createMetricAssesment("LCP", cruxRecord.Record.Metrics.LCP),
+	}
+}
+
 func main() {
 	args := ReadArgs()
 
-	var cruxRecord CruxRecord
-	var err error
-
-	cruxRecord, err = GetCruxData(args.apiKey, args.url, args.verbose)
+	cruxRecord, err := GetCruxData(args.apiKey, args.url, args.verbose)
 	if err != nil {
 		fmt.Printf("Failed to get CrUX data: %s\n", err)
 		os.Exit(1)
 	}
 
+	metricResults := assessCoreWebVitals(cruxRecord)
+
 	fmt.Println()
 	fmt.Printf("%10s: %5v %10v %7s\n", "Metric", "P75", "Threshold", "Status")
-
-	var status bool
-	status, err = assessMetric(cruxRecord.Record.Metrics.CLS)
-	if err != nil {
-		fmt.Printf("Failed to asses CLS: %s\n", err)
-		os.Exit(1)
+	for _, val := range metricResults {
+		fmt.Printf("%10s: %5v %10v %7s\n", val.name, val.p75, val.threshold, val.score)
 	}
-
-	printMetric(cruxRecord.Record.Metrics.CLS, status)
-
-	status, err = assessMetric(cruxRecord.Record.Metrics.FID)
-	if err != nil {
-		fmt.Printf("Failed to asses FID: %s\n", err)
-		os.Exit(1)
-	}
-	printMetric(cruxRecord.Record.Metrics.FID, status)
-
-	status, err = assessMetric(cruxRecord.Record.Metrics.LCP)
-	if err != nil {
-		fmt.Printf("Failed to asses LCP: %s\n", err)
-		os.Exit(1)
-	}
-
-	printMetric(cruxRecord.Record.Metrics.LCP, status)
-}
-
-func printMetric(metric Metric, status bool) {
-	var result = "fail"
-	if status {
-		result = "pass"
-	}
-
-	fmt.Printf("%10s: %5v %10v %7s\n", "CLS", metric.Percentiles.P75, metric.Histogram[0].End, result)
 }
