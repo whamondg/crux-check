141973b76678097e353276ac09ba22c4a899bd32
whamondg <gordon.whamond@googlemail.com>
2020-11-22 19:07:02 +0000

    set timeout and useragent

--- cruxfetch.go
+++ cruxfetch.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
 /*
@@ -51,36 +53,47 @@ GetCruxData calls the CrUX api to retrieve user experience data
 func GetCruxData(apiKey string, target string, verbose bool) (CruxRecord, error) {
 	cruxRecord := CruxRecord{}
 
-	fmt.Printf("Checking CrUX data for %s\n", target)
-
 	cruxAPI := fmt.Sprintf("https://chromeuxreport.googleapis.com/v1/records:queryRecord?key=%s", apiKey)
 	requestBody, _ := json.Marshal(map[string]string{"url": target})
-	response, err := http.Post(cruxAPI, "application/json", bytes.NewBuffer(requestBody))
+	req, err := http.NewRequest(http.MethodPost, cruxAPI, bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", "crux-check")
+
+	httpClient := http.Client{Timeout: time.Second * 2}
+	if verbose {
+		fmt.Println("Sending Request:")
+		fmt.Printf("%+v\n\n", req)
+	}
+
+	response, err := httpClient.Do(req)
 	if err != nil {
 		return cruxRecord, fmt.Errorf("The HTTP request failed with error %s", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-
-		if response.StatusCode != 200 {
-			return cruxRecord, fmt.Errorf("Received an error while fetching CrUX data\n %s", string(data))
-		}
-
-		if verbose {
-			fmt.Printf("Received CrUX data\n")
-			fmt.Println(string(data))
-		}
-
-		jsonErr := json.Unmarshal(data, &cruxRecord)
-		if jsonErr != nil {
-			return cruxRecord, fmt.Errorf("JSON Unmarshalling failed with error %s", err)
-		}
-
-		if verbose {
-			fmt.Printf("Parsed JSON into Structs\n")
-			fmt.Printf("%+v\n", cruxRecord)
-		}
-
-		return cruxRecord, nil
 	}
+
+	data, _ := ioutil.ReadAll(response.Body)
+
+	if response.StatusCode != 200 {
+		return cruxRecord, fmt.Errorf("Received an error while fetching CrUX data\n %s", string(data))
+	}
+
+	if verbose {
+		fmt.Println("Received CrUX data:")
+		fmt.Println(string(data))
+	}
+
+	jsonErr := json.Unmarshal(data, &cruxRecord)
+	if jsonErr != nil {
+		return cruxRecord, fmt.Errorf("JSON Unmarshalling failed with error %s", err)
+	}
+
+	if verbose {
+		fmt.Println("Parsed JSON into Structs:")
+		fmt.Printf("%+v\n", cruxRecord)
+	}
+
+	return cruxRecord, nil
 }
